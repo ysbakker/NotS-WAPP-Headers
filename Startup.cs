67c36affbd95209471741650c46991b434a2b041
Yorrick Bakker <y.bakker@me.com>
2021-05-23 11:41:33 +0200

    Remove Swagger

--- Startup.cs
+++ Startup.cs
@@ -59,11 +59,6 @@ namespace HeaderDemo
                 options.IncludeSubDomains = true;
                 options.MaxAge = TimeSpan.FromDays(30);
             });
-            
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "HeaderDemo", Version = "v1"});
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,8 +67,6 @@ namespace HeaderDemo
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HeaderDemo v1"));
             }
 
             app.UseRouting();
